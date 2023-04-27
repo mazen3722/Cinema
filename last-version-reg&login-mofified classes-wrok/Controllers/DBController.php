05042d0d7cd7690b7765ede677a8b87fa228024d
mezonafe3 <105220171+mezonafe3@users.noreply.github.com>
2023-04-27 18:44:14 +0200

    lastlast

---
+++ last-version-reg&login-mofified classes-wrok/Controllers/DBController.php
@@ -0,0 +1,83 @@
+<?php 
+
+class DBController
+{
+    
+   protected $dbHost="localhost";
+   protected $dbUser="root";
+   protected $dbPassword="";
+   protected $dbName="movie_site";
+   protected $connection;
+
+    public function openConnection()
+    {
+        $this->connection=new mysqli($this->dbHost,$this->dbUser,$this->dbPassword,$this->dbName);
+        if($this->connection->connect_error)
+        {
+            echo " Error in Connection : ".$this->connection->connect_error;
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public function closeConnection()
+    {
+        if($this->connection)
+        {
+            $this->connection->close();
+        }
+        else
+        {
+            echo "Connection is not opened";
+        }
+    }
+
+    public function select($qry)
+    {
+        $result=$this->connection->query($qry);
+        if(!$result)
+        {
+            echo "Error : ".mysqli_error($this->connection);
+            return false;
+        }
+        else
+        {
+             return $result->fetch_all(MYSQLI_ASSOC);
+        }
+
+    }
+    public function insert($qry)
+    {
+        $result=$this->connection->query($qry);
+        if(!$result)
+        {
+            echo "Error : ".mysqli_error($this->connection);
+            return false;
+        }
+        else
+        {
+             return $this->connection->insert_id;//last raw ibserted id
+        }
+
+    }
+    public function delete($qry)
+    {
+        $result=$this->connection->query($qry);
+        if(!$result)
+        {
+            echo "Error : ".mysqli_error($this->connection);
+            return false;
+        }
+        else
+        {
+             return $result;
+        }
+
+    }
+
+}
+
+?>
